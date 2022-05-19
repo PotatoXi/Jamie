62bb7afea47ae6ff307dbca3fe5fc979e77c4d60
Zhao Jianming <45778832+PotatoXi@users.noreply.github.com>
2022-05-19 23:18:20 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,14 +18,11 @@ setuptools.setup(
     license="BSD",
     url="https://github.com/PotatoXi/Jamie",
 
-    project_urls={
-        "Bug Tracker": "https://github.com/ni1o1/transbigdata/issues",
-    },
     install_requires=[
         "numpy", "pandas", "shapely", "geopandas", "scipy", "matplotlib"
     ],
 
     package_dir={'Jamie': 'src/jamie_hello'},
-    packages=['jamie'],
+    packages=['Jamie'],
     python_requires=">=3.6",
 )
