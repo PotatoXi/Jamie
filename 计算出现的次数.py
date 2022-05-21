0087b4f9e63a54981fea3c209d90322146bf85df
Zhao Jianming <45778832+PotatoXi@users.noreply.github.com>
2022-05-21 09:53:04 +0800

    Add files via upload

---
+++ 计算出现的次数.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 19 16:34:28 2021
+
+@author: JmZhao
+
+E-mail: jmzhao29@gmail.com
+
+Progress is the activity of today and the assurance of tomorrow ！
+
+"""
+import numpy as np
+import random
+from collections import Counter
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+
+
+#光谱图
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import fmin,fminbound
+from scipy.interpolate import make_interp_spline
+import math
+import os
+
+#数据
+re = open('H:/桌面/1.txt')#归一化数据
+r = Counter(re)
+print('统计：',r)
